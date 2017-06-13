aca2bc23867a38c7d2703fb045ada811f69a49ef
clarenceangel <knowplaceapps@icloud.com>
2017-06-13 23:10:26 +0100

    Add files via upload

---
+++ tables.py
@@ -0,0 +1,34 @@
+from tkinter import *
+import csv
+
+class tables:
+    def createStandardTable(f,window):
+        handle = csv.reader(f)
+        length = len(next(handle))
+
+        sizes = [0] * length
+        for record in handle:
+            
+            for p,column in enumerate(record):
+                if len(column) > sizes[p]:
+                    sizes[p] = len(column)+3
+                    
+        f.seek(0)
+        trow = 0
+        table = Frame(window)
+        for record in handle:
+            for w,column in enumerate(record):
+                if w == 0: size = 5
+                else: size = 30
+                Label(table,text=column,width=sizes[w],borderwidth=2,relief="groove",justify=LEFT,anchor=W, background='white').grid(column=w,row=trow,sticky=W)
+            
+            trow+=1
+
+        
+        return table
+
+    
+
+
+        
+            
